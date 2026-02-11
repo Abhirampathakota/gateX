0214a6d54260274c25e286455745f066ce8574b5
abhirampathakota <abhirampathakota@gmail.com>
2026-02-11 22:55:19 +0530

    final

--- core/settings.py
+++ core/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-1@6&98j_0n)9vvt#6@5u&7l*tq84^gkw+8+0l_104q(_mqu=b('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['gatex-w9hs.onrender.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = []
 
 
 # Application definition
